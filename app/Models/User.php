edbac1f1cc0137721b499f432f4f3fa5bc11acc5
杨伟杰 <sia-fl@outlook.com>
2023-08-02 08:36:32 +0800

    update: ide-helper ignore & make model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,9 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @mixin IdeHelperUser
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
