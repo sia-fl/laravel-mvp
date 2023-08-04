cbbcc684279c27352a1144b81d67ebe7cf62a567
杨伟杰 <sia-fl@outlook.com>
2023-08-04 08:21:02 +0800

    update: 添加日志状态字段颜色

--- app/Enum/TagMoveLog/TagMoveLogStateEnum.php
+++ app/Enum/TagMoveLog/TagMoveLogStateEnum.php
@@ -2,9 +2,10 @@
 
 namespace App\Enum\TagMoveLog;
 
+use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasLabel;
 
-enum TagMoveLogStateEnum: string implements HasLabel
+enum TagMoveLogStateEnum: string implements HasLabel, HasColor
 {
     // 移动
     case Yd = 'yd';
@@ -19,6 +20,15 @@ enum TagMoveLogStateEnum: string implements HasLabel
             self::Yd => '移动',
             self::Fx => '发现',
             self::Xs => '消失',
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::Yd => 'gray',
+            self::Fx => 'info',
+            self::Xs => 'danger',
         };
     }
 }
