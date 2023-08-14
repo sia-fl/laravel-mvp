eaad855e4ce5aad9944fb38eac02447f708574a2
杨伟杰 <sia-fl@outlook.com>
2023-08-15 02:40:17 +0800

    fix: 日志不应该可以创建

--- app/Filament/Resources/Tag/TagMoveLogResource.php
+++ app/Filament/Resources/Tag/TagMoveLogResource.php
@@ -18,17 +18,9 @@ class TagMoveLogResource extends Resource
 
     protected static ?string $label = '轨迹日志';
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
 
     protected static ?int $navigationSort = 6;
-
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                //
-            ]);
-    }
 
     public static function table(Table $table): Table
     {
@@ -45,18 +37,12 @@ class TagMoveLogResource extends Resource
             ])
             ->filters([
                 //
-            ])
-            ->actions([
-                //                Tables\Actions\EditAction::make(),
-            ])->actionsPosition(ActionsPosition::BeforeCells)
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ])
-            ->emptyStateActions([
-                //                Tables\Actions\CreateAction::make(),
             ]);
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
     }
 
     public static function getRelations(): array
@@ -70,8 +56,6 @@ class TagMoveLogResource extends Resource
     {
         return [
             'index' => Pages\ListTagMoveLogs::route('/'),
-            'create' => Pages\CreateTagMoveLog::route('/create'),
-            'edit' => Pages\EditTagMoveLog::route('/{record}/edit'),
         ];
     }
 }
